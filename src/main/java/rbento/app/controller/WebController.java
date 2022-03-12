b45bc9fa8fe5055595160657e3b3a9a60401410a
Rodrigo Bento <rodrigo.bento@gmail.com>
2022-03-12 00:31:28 -0500

    Add LOG and REQUEST_COUNTER to WebController

--- src/main/java/rbento/app/controller/WebController.java
+++ src/main/java/rbento/app/controller/WebController.java
@@ -1,20 +1,37 @@
 package rbento.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("client", System.getenv("APP_CLIENT"));
-        model.addAttribute("version", System.getenv("APP_VERSION"));
-		model.addAttribute("author", System.getenv("APP_AUTHOR"));
-		model.addAttribute("year", LocalDate.now().getYear());
-        return "index";
-    }
+	private static final Logger LOG = LoggerFactory.getLogger(WebController.class);
+	private static final AtomicInteger REQUEST_COUNTER = new AtomicInteger();
+
+	@GetMapping("/")
+	public String index(Model model) {
+
+		REQUEST_COUNTER.incrementAndGet();
+
+		var author = System.getenv("APP_AUTHOR");
+		var client = System.getenv("APP_CLIENT");
+		var version = System.getenv("APP_VERSION");
+		var year = LocalDate.now().getYear();
+
+		LOG.info("index: { requests: {}, author: {}, client: {}, version: {}, year: {}", REQUEST_COUNTER, author, client, version, year);
+
+		model.addAttribute("author", author);
+		model.addAttribute("client", author);
+		model.addAttribute("version", version);
+		model.addAttribute("year", year);
+
+		return "index";
+	}
 }
